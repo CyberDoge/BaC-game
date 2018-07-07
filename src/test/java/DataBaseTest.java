3b31120f2ec13ffbd76c5c6096c0dc62f159369d
CyberDoge <tagirman998@gmail.com>
2018-07-07 16:59:09 +0400

    starting add test

---
+++ src/test/java/DataBaseTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DataBaseTest {
+    private Connection connection;
+/*
+    @BeforeAll
+    public void init() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bac_game", "root", "123");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("init");
+    }*/
+
+    @Test
+    public void test() {
+        assertEquals(1, 3, "norm");
+    }
+}
